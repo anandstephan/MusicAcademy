d88b8ba48c434fc20a0c665e433d6703754f05a5
Anand Stephan <anandstephan98@gmail.com>
2024-03-25 15:49:28 +0530

    fix:typo error on import

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link"
-import { Spotlight } from "./ui/SpotLight"
+import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border";
 
 const HeroSection = () =>{
